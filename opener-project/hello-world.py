f022773823998106e64eabd87b028b124284db38
Sydney Maples <smaples@stanford.edu>
2015-04-14 14:15:57 -0700

    Delete hello-world.py

--- opener-project/hello-world.py
+++
@@ -1,7 +0,0 @@
-print("Hello world")
-myname = "Sydney"
-print("My name is", myname)
-myage = 50
-print("I am", myage, "years old")
-print("That is roughly", 50 * 365, "days")
-print("Or", 50 * 365 * 24 * 60 * 60, "seconds")
