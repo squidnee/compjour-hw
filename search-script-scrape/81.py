c982082689acec1702f626bb39c6c219921a0ff5
Sydney <smaples@stanford.edu>
2015-06-08 17:49:29 -0700

    adding 81 (may not work?)

--- search-script-scrape/81.py
+++ search-script-scrape/81.py
@@ -3,3 +3,9 @@
 import requests as r
 import bs4
 url = r.get('http://www.transtats.bts.gov/airports.asp')
+soup = bs4.BeautifulSoup(url.text)
+count=0
+select = soup.select("select#End_YearMonth option.selected")
+for s in select:
+	count+=1
+print(select[count].text)
