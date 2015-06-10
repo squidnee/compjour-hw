017e5308a3fcbc0bffa89a5a3ac01a89e5373a2a
Sydney <smaples@stanford.edu>
2015-06-10 03:16:25 -0700

    updating app.py

--- final-project/app.py
+++ final-project/app.py
@@ -3,13 +3,27 @@ from controllers import index_controller, sends_controller
 
 app = Flask(__name__)
 
-bool success = true
-bool fail = false
+data = index_controller.get_data()
+
+def total_items(data):
+	items = list(data)
+	return len(items)
+
+def get_success_rate(data):
+	success = sum([d['requester_received_pizza'] for d in data])
+	rate = 100.0 * (success / len(list(data)))
+	return rate
 
 @app.route("/")
 def index():
-	data = controller.load()
-	return render_template('index.html', data=data)
+	post_status = index_controller.post_status(data)
+	karma = index_controller.user_karma(data)
+	chars = index_controller.num_chars_in_post(data)
+	edited = index_controller.edited(data)
+	age = index_controller.acc_age(data)
+	items = total_items(data)
+	rate = get_success_rate(data)
+	return render_template('index.html', data=data, post_status=post_status, karma=karma, chars=chars, edited=edited, age=age, items=items, rate=rate)
 
 @app.route("/search", methods=['POST'])
 def search():
